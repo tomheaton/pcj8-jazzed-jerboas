0c1e0025f8aba3b653669d3d1902afbe33a6158a
Yubo Wang <yubowang2007@gmail.com>
2021-07-10 22:56:54 +0100

    impoved animation

--- client/credits.py
+++ client/credits.py
@@ -12,9 +12,9 @@ from rich.panel import Panel
 
 console: Console = Console()
 
-height = ['bottom', 'middle', 'top']
+height: list = ['bottom', 'middle', 'top']
 
-team_name = [Text.assemble((
+team_name: list = [Text.assemble((
     r"""
 :                |$$$$$$$$| |$$| |$$| |$$$$$$|               :
 :                   |$$|    |$$| |$$| |$|                    :
@@ -38,182 +38,222 @@ team_name = [Text.assemble((
 :|$$$$$| |$$$$$| |$|  \$\ |$$$$$/  \$$$$$/  |$| |$|  |$$$$$/ :
 """, 'bold purple'), justify='center')]
 
-jazzed_jerboas = ['[green]tomheaton[/]', '[purple]MikeNoCap[/]', '[purple]HiPeople21[/]',
-                  '[purple]TahU28[/]', '[purple]ryoflux[/]', '[purple]b-a-b-i-s[/]']
+jazzed_jerboas: list = ['[green]tomheaton[/]', '[purple]MikeNoCap[/]', '[purple]HiPeople21[/]',
+                        '[purple]TahU28[/]', '[purple]ryoflux[/]', '[purple]b-a-b-i-s[/]']
 
-message = 'Placeholder'
+message: str = 'Placeholder'
 
 
 def credits() -> None:
     with console.screen() as screen:
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"[purple]--------------------[/purple][magenta]This was built by:[/magenta][purple]--------------------[/purple]", justify="center"),
             vertical=height[0],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"[purple]--------------------[/purple][magenta]This was built by:[/magenta][purple]--------------------[/purple]\n{team_name[0]}", justify="center"),
             vertical=height[0],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"[purple]--------------------[/purple][magenta]This was built by:[/magenta][purple]--------------------[/purple]\n{team_name[0]}\n{team_name[1]}", justify="center"),
             vertical=height[0],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"[purple]--------------------[/purple][magenta]This was built by:[/magenta][purple]--------------------[/purple]\n{team_name[0]}\n{team_name[1]}\n{team_name[2]}", justify="center"),
             vertical=height[0],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"[purple]--------------------[/purple][magenta]This was built by:[/magenta][purple]--------------------[/purple]\n{team_name[0]}\n{team_name[1]}\n{team_name[2]}\n{jazzed_jerboas[0]}", justify="center"),
             vertical=height[0],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"[purple]--------------------[/purple][magenta]This was built by:[/magenta][purple]--------------------[/purple]\n{team_name[0]}\n{team_name[1]}\n{team_name[2]}\n{jazzed_jerboas[0]}\n{jazzed_jerboas[1]}", justify="center"),
             vertical=height[0],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"[purple]--------------------[/purple][magenta]This was built by:[/magenta][purple]--------------------[/purple]\n{team_name[0]}\n{team_name[1]}\n{team_name[2]}\n{jazzed_jerboas[0]}\n{jazzed_jerboas[1]}\n{jazzed_jerboas[2]}", justify="center"),
             vertical=height[0],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
-                f"{team_name[0]}\n{team_name[1]}\n{team_name[2]}\n{jazzed_jerboas[0]}\n{jazzed_jerboas[1]}\n{jazzed_jerboas[2]}\n{jazzed_jerboas[3]}", justify="center"),
+                f"[purple]--------------------[/purple][magenta]This was built by:[/magenta][purple]--------------------[/purple]\n{team_name[0]}\n{team_name[1]}\n{team_name[2]}\n{jazzed_jerboas[0]}\n{jazzed_jerboas[1]}\n{jazzed_jerboas[2]}\n{jazzed_jerboas[3]}", justify="center"),
+            vertical=height[0],
+        )
+        screen.update(Panel(text))
+        sleep(0.5)
+
+        text: Text = Align.center(
+            Text.from_markup(
+                f"[purple]--------------------[/purple][magenta]This was built by:[/magenta][purple]--------------------[/purple]\n{team_name[0]}\n{team_name[1]}\n{team_name[2]}\n{jazzed_jerboas[0]}\n{jazzed_jerboas[1]}\n{jazzed_jerboas[2]}\n{jazzed_jerboas[3]}\n{jazzed_jerboas[4]}", justify="center"),
+            vertical=height[0],
+        )
+        screen.update(Panel(text))
+        sleep(0.5)
+
+        text: Text = Align.center(
+            Text.from_markup(
+                f"[purple]--------------------[/purple][magenta]This was built by:[/magenta][purple]--------------------[/purple]\n{team_name[0]}\n{team_name[1]}\n{team_name[2]}\n{jazzed_jerboas[0]}\n{jazzed_jerboas[1]}\n{jazzed_jerboas[2]}\n{jazzed_jerboas[3]}\n{jazzed_jerboas[4]}\n{jazzed_jerboas[5]}", justify="center"),
+            vertical=height[0],
+        )
+        screen.update(Panel(text))
+        sleep(0.5)
+
+        text: Text = Align.center(
+            Text.from_markup(
+                f"[purple]--------------------[/purple][magenta]This was built by:[/magenta][purple]--------------------[/purple]\n{team_name[0]}\n{team_name[1]}\n{team_name[2]}{jazzed_jerboas[0]}\n{jazzed_jerboas[1]}\n{jazzed_jerboas[2]}\n{jazzed_jerboas[3]}\n{jazzed_jerboas[4]}\n{jazzed_jerboas[5]}", justify="center"),
+            vertical=height[0],
+        )
+        screen.update(Panel(text))
+        sleep(0.5)
+
+        text: Text = Align.center(
+            Text.from_markup(
+                f"[purple]--------------------[/purple][magenta]This was built by:[/magenta][purple]--------------------[/purple]\n{team_name[0]}\n{team_name[1]}\n{team_name[2]}{jazzed_jerboas[0]}\n{jazzed_jerboas[1]}\n{jazzed_jerboas[2]}\n{jazzed_jerboas[3]}\n{jazzed_jerboas[4]}\n{jazzed_jerboas[5]}", justify="center"),
+            vertical=height[1],
+        )
+        screen.update(Panel(text))
+        sleep(0.5)
+
+        text: Text = Align.center(
+            Text.from_markup(
+                f"{team_name[0]}\n{team_name[1]}\n{team_name[2]}{jazzed_jerboas[0]}\n{jazzed_jerboas[1]}\n{jazzed_jerboas[2]}\n{jazzed_jerboas[3]}\n{jazzed_jerboas[4]}\n{jazzed_jerboas[5]}", justify="center"),
             vertical=height[2],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
-                f"{team_name[1]}\n{team_name[2]}\n{jazzed_jerboas[0]}\n{jazzed_jerboas[1]}\n{jazzed_jerboas[2]}\n{jazzed_jerboas[3]}\n{jazzed_jerboas[4]}", justify="center"),
+                f"{team_name[1]}\n{team_name[2]}{jazzed_jerboas[0]}\n{jazzed_jerboas[1]}\n{jazzed_jerboas[2]}\n{jazzed_jerboas[3]}\n{jazzed_jerboas[4]}\n{jazzed_jerboas[5]}", justify="center"),
             vertical=height[2],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
-                f"{team_name[2]}\n{jazzed_jerboas[0]}\n{jazzed_jerboas[1]}\n{jazzed_jerboas[2]}\n{jazzed_jerboas[3]}\n{jazzed_jerboas[4]}\n{jazzed_jerboas[5]}", justify="center"),
+                f"{team_name[2]}{jazzed_jerboas[0]}\n{jazzed_jerboas[1]}\n{jazzed_jerboas[2]}\n{jazzed_jerboas[3]}\n{jazzed_jerboas[4]}\n{jazzed_jerboas[5]}", justify="center"),
             vertical=height[2],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"{jazzed_jerboas[0]}\n{jazzed_jerboas[1]}\n{jazzed_jerboas[2]}\n{jazzed_jerboas[3]}\n{jazzed_jerboas[4]}\n{jazzed_jerboas[5]}", justify="center"),
             vertical=height[2],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"{jazzed_jerboas[1]}\n{jazzed_jerboas[2]}\n{jazzed_jerboas[3]}\n{jazzed_jerboas[4]}\n{jazzed_jerboas[5]}", justify="center"),
             vertical=height[2],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"{jazzed_jerboas[2]}\n{jazzed_jerboas[3]}\n{jazzed_jerboas[4]}\n{jazzed_jerboas[5]}", justify="center"),
             vertical=height[2],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"{jazzed_jerboas[3]}\n{jazzed_jerboas[4]}\n{jazzed_jerboas[5]}", justify="center"),
             vertical=height[2],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"{jazzed_jerboas[4]}\n{jazzed_jerboas[5]}", justify="center"),
             vertical=height[2],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"{jazzed_jerboas[5]}", justify="center"),
             vertical=height[2],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"", justify="center"),
             vertical=height[2],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"{message}", justify="center"),
             vertical=height[0],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"{message}", justify="center"),
             vertical=height[1],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"{message}", justify="center"),
             vertical=height[2],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
-        text = Align.center(
+        text: Text = Align.center(
             Text.from_markup(
                 f"", justify="center"),
             vertical=height[2],
         )
         screen.update(Panel(text))
-        sleep(1)
+        sleep(0.5)
 
 
 credits()
