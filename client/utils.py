638135a6c01529bc9756d99b00db6b3742a41592
TahU28 <77585518+TahU28@users.noreply.github.com>
2021-07-11 16:07:44 +0100

    added preference system

--- client/utils.py
+++ client/utils.py
@@ -10,8 +10,52 @@ console: Console = Console()
 
 
 class Preferences:
-    def __init__(self):
-        pass
+    """
+    This needs to be passed onto the 'User' class as it sets the default preferences:
+    'sampleuser = User("samplename", "password", default_preference)'
+
+    default_preference = {
+        "Name Colour" : "bold cyan",
+        "Message Colour" : "bold white",
+        "Border Colour" : "bold magenta"
+    }
+
+    To change the preference, this is used:
+    'sampleuser.preferences = Preferences.settings(sampleuser.preferences)'
+    *Note: this only allows for one settings to be changed, so a while loop would need
+    to be used to allow multiple settings be to changed.
+    """
+
+    def template_message(self):
+        """
+        Function which prints out a preview of how the users box is going to look.
+        """
+        console.print(Panel("This is a template message.", title = ("["+self["Name Colour"]+"]") + "Title" + "[/]", border_style = self["Border Colour"],style = self["Message Colour"], width = 25, padding = 1, title_align = "left"))
+
+
+    def settings(self):
+        """
+        Function which allows the user to change thier settings. Allows hex and named
+        colour values and returns then updated preference.
+        """
+        Preferences.template_message(self)
+        val_to_be_changed: str = Prompt.ask("\n🖌️    What would you like to change?    ", choices = ["Border Colour", "Name Colour", "Message Colour", "BACK"])
+
+        if val_to_be_changed == "BACK":
+            exit()
+
+        else:
+            while True:
+                colour: str = Prompt.ask("🎨   Enter the name or hex value of your desired colour   ")
+                try:
+                    test: str = console.print("", style = colour)
+                except:
+                    console.print("❌   Please enter a valid colour!\n")
+                else:
+                    previous_setting: str = self[val_to_be_changed]
+                    self[val_to_be_changed] = "bold " + str(colour)
+                    console.print("✔️   Updated successfully!\n")
+                    return self 
 
 
 class User:
