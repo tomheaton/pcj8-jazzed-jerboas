dc16e711266d611e56c25d9c9e38d8e7802873a7
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-07-16 17:54:12 +0100

    added TODOs for collating all the components

--- client/client.py
+++ client/client.py
@@ -2,7 +2,7 @@
 import asyncio
 import socketio
 from rich.console import Console
-from rich.prompt import Prompt
+from rich.prompt import Prompt, Confirm, IntPrompt
 
 sio = socketio.AsyncClient()
 console = Console()
@@ -47,11 +47,40 @@ async def main():
 async def console_loop():
     # TODO: log the user in/make an account
     if CONNECTED:
-        name: str = Prompt.ask("enter username")
-        globals().update(USERNAME=name)
-        room_name: str = Prompt.ask("enter room name")
-        globals().update(ROOM=room_name)
-        await sio.emit("join_room", {"username": USERNAME, "room_name": ROOM})
+        if Confirm.ask("returning user?"):
+            # TODO: let user sign in to an account.
+            username: str = Prompt.ask("enter username")
+            password: str = Prompt.ask("enter password")
+            # IF SUCCESS, create account and log them in
+            globals().update(USERNAME=username)
+            room_name: str = Prompt.ask("enter room name")
+            globals().update(ROOM=room_name)
+            await sio.emit("join_room", {"username": USERNAME, "room_name": ROOM})
+        else:
+            # TODO: let user create an account.
+            console.print("time to sign-up!")
+            username: str = Prompt.ask("enter username")
+            password: str = Prompt.ask("enter password")
+            # IF SUCCESS, create account and log them in
+            globals().update(USERNAME=username)
+            room_name: str = Prompt.ask("enter room name")
+            globals().update(ROOM=room_name)
+            await sio.emit("join_room", {"username": USERNAME, "room_name": ROOM})
+
+        await asyncio.sleep(0.01)
+
+        if Confirm.ask("join a chatroom?"):
+            # TODO: let user join a chatroom.
+            chatroom_name: str = Prompt.ask("enter chatroom to join")
+            # TODO: check chatroom before joining. (do not let user create one by joining)
+        if Confirm.ask("create a chatroom?"):
+            # TODO: let user create a chatroom.
+            chatroom_name: str = Prompt.ask("enter name for your chatroom")
+            private: bool = Confirm.ask("private?")
+            # TODO: check capacity input.
+            capacity: int = IntPrompt.ask("enter room size")
+
+        await asyncio.sleep(0.01)
 
     while True and CONNECTED:
         await asyncio.sleep(2)
