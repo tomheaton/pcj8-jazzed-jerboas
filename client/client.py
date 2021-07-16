2944a2fe4902c8f3a29624bf99abfe39b1d5c3a9
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-07-16 15:43:06 +0100

    added new client and server

--- client/client.py
+++ client/client.py
@@ -1,1 +1,59 @@
 # Client
+import asyncio
+import socketio
+from rich.console import Console
+from rich.prompt import Prompt
+
+sio = socketio.AsyncClient()
+console = Console()
+CONNECTED: bool = False
+USERNAME = ""
+ROOM = ""
+
+
+@sio.event
+async def connect():
+    print('[CLIENT]: connected to server')
+    globals().update(CONNECTED=True)
+
+
+@sio.event
+async def disconnect():
+    print('[CLIENT]: disconnected from server')
+    globals().update(CONNECTED=False)
+
+
+@sio.event
+async def send_message(sid, data):
+    print(f'[CLIENT]: message sent {sid}, data: {data}')
+
+
+@sio.event
+async def receive_message(data):
+    print(f"[{data['username']}]: {data['message']}")
+
+
+async def main():
+    print("[CLIENT]: Starting connection...")
+    await sio.connect('http://localhost:8080')
+    await asyncio.sleep(2)
+    print("[CLIENT]: Running console...")
+    await console_loop()
+
+
+async def console_loop():
+    if CONNECTED:
+        name: str = Prompt.ask("enter username")
+        globals().update(USERNAME=name)
+        room_name: str = Prompt.ask("enter room name")
+        globals().update(ROOM=room_name)
+        await sio.emit("join_room", {"username": USERNAME, "room_name": ROOM})
+
+    while True & CONNECTED:
+        await asyncio.sleep(2)
+        message: str = Prompt.ask("enter message")
+        await asyncio.sleep(0.01)
+        await sio.emit("send_message", {"username": USERNAME, "message": message, "room_name": ROOM})
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- server/server.py
+++ server/server.py
@@ -1,1 +1,44 @@
 # Server
+import socketio
+from aiohttp import web
+
+sio = socketio.AsyncServer(async_mode="aiohttp")
+app = web.Application()
+sio.attach(app)
+
+
+@sio.event
+async def connect(sid):
+    print(f"[SERVER]: connect {sid}")
+
+
+@sio.event
+async def disconnect(sid):
+    print(f"[SERVER]: disconnect {sid}")
+
+
+@sio.event
+async def join_room(sid, data):
+    print(f"{data['username']} joined to {data['room_name']}")
+    sio.enter_room(sid, data['room_name'])
+
+
+@sio.event
+async def leave_room(sid, data):
+    print(f"{data['username']} left {data['room_name']}")
+    sio.leave_room(sid, data['room_name'])
+
+
+@sio.event
+async def send_message(sid, data):
+    print(f"[SERVER]: send message {sid}, data: {data}")
+    await sio.emit("receive_message", data, room=data["room_name"])
+
+
+@sio.event
+async def receive_message(sid, data):
+    print(f"[SERVER]: receive message {sid}, data: {data}")
+
+
+if __name__ == "__main__":
+    web.run_app(app)
