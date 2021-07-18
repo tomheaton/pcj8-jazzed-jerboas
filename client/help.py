e43109245abf07e76c3eba60247b9f1a899e51db
HiPeople21 <86291833+HiPeople21@users.noreply.github.com>
2021-07-18 11:34:53 +0100

    Update help.py

--- client/help.py
+++ client/help.py
@@ -81,19 +81,13 @@ def help_start_page(user):
     else:
         main_style = "bold cyan"
     clear()
-    paragraph = "[bright_white]ThaBox is a social media platform in which the conversations " \
-                "\nbetween users are for the users only. Not a single message is " \
-                "\nsaved, giving you the utmost privacy. Chat with " \
-                "\nstrangers in public boxes, or have a blast with your friends " \
-                "\nin private boxes. " \
-                "\n\n[/][bold grey58]This program was created for the 'Python Discord 2021 CodeJam'.\n[/]"
 
     paragraph = """
     [bright_white]ThaBox is a social media platform in which the conversations 
     between users are for the users only. Not a single message is
     saved, giving you the utmost privacy. Chat with
-    strangers in public boxes, or have a blast with your friends
-    in private boxes.
+    strangers in boxes, or have a blast with your friends
+    in boxes.
     
     [/][bold grey58]This program was created for the 'Python Discord 2021 CodeJam'.\n[/]
     """
@@ -111,19 +105,11 @@ def help_boxes(user):
     else:
         main_style = "bold cyan"
     clear()
-    paragraph = "[bright_white]There are two types of boxes available - a private and a public" \
-                "\nbox. When creating a public box, you get a session ID, which" \
-                "\nothers can find in a list of public boxes and join. However, when" \
-                "\ncreating a private box, you get a session ID and have to set a" \
-                "\npassword for the box. Any user with the session ID and password" \
-                "\ncan join your box.\n[/]"
+
     paragraph = """
-    [bright_white]There are two types of boxes available - a private and a public
-    box. When creating a public box, you get a session ID, which
-    others can find in a list of public boxes and join. However, when
-    creating a private box, you get a session ID and have to set a
-    password for the box. Any user with the session ID and password
-    can join your box.
+    [bright_white]When creating a box, it will check if a box of that 
+    name already exists, and if it doesn't one will be created. However,
+    if it does exist, you will join the box and be able to start talking.
     [/]
     """
 
@@ -140,21 +126,14 @@ def help_messages(user):
     else:
         main_style = "bold cyan"
     clear()
-    paragraph = "[bright_white]Messages can be sent to another person in the same box, by simply"\
-                "\ntyping it out when you receive the prompt. Only 1 person in the"\
-                "\nbox can type at once, so you may need to wait until someone else"\
-                "\nhas finished."\
-                "\n\nMessages that you receive are available for some seconds."\
-                "\nmessage has been sent. Past message are not stored and can't be "\
-                "\nretrieved.\n[/]"
+
     paragraph = """
     [bright_white]Messages can be sent to another person in the same box, by simply
-    typing it out when you receive the prompt. Only 1 person in the
-    box can type at once, so you may need to wait until someone else
-    has finished.
+    typing it out when you receive the prompt. However you can only see the other 
+    people's messages after you have finished typing.
     
-    Messages that you receive are available for some seconds.
-    message has been sent. Past message are not stored and can't be
+    Messages that you receive are available for some seconds after
+    the message has been sent. Message are not stored and can't be
     retrieved.
     [/]
     """
@@ -172,12 +151,9 @@ def help_preference(user):
     else:
         main_style = "bold cyan"
     clear()
-    paragraph = "[bright_white]The colour scheme of your box can be changed in the"\
-                "\nPreferences page once logged in. A valid colour name or a hex-value (#xxxxxx)"\
-                "\ncan be used to personalise different parts of your box. [/]\n"
     paragraph = """
     [bright_white]The colour scheme of your box can be changed in the
-    Preferences page once logged in. A valid colour name or a hex-value (#xxxxxx)
+    preferences page once logged in. A valid colour name or a hex-value (#xxxxxx)
     can be used to personalise different parts of your box. [/]
     
     """
@@ -187,3 +163,4 @@ def help_preference(user):
     █▀▀ █▀▄ ██▄ █▀  ██▄ █▀▄ ██▄ █ ▀█ █▄▄ ██▄ ▄█[/]\n""", align="center", )))
     console.print((Align(paragraph, align="center")))
     help_navigation("Pref", user)
+
